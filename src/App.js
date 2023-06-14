2709a7146624b6fcd65728c984fb450b3b48a13b
Rosaverde <rosaverde.ag@gmail.com>
2023-06-14 19:10:57 +0000

    Update Light/Dark mode implementation

--- src/App.js
+++ src/App.js
@@ -2,12 +2,17 @@ import React from "react";
 import Navbar from "./components/Navbar";
 import Main from "./components/Main";
 
-export default function App()
-{
-    return(
+export default function App() {
+    const [darkMode, setDarkMode] = React.useState(false)
+
+    function toggleDarkMode() {
+        setDarkMode(prevState => !prevState)
+    }
+
+    return (
         <div className="container">
-            <Navbar />
-            <Main />
+            <Navbar toggleDarkMode={toggleDarkMode} darkMode={darkMode} />
+            <Main darkMode={darkMode} />
         </div>
     )
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,12 +1,21 @@
 import React from "react";
+import logo from "../images/reactjs-icon 1.png"
 
-export default function Navbar() 
-{
-    return(
-        <nav>
-            <img className="nav-logo" src="https://logo-download.com/wp-content/data/images/png/React-logo.png" />
+export default function Navbar(props) {
+
+    return (
+        <nav className={props.darkMode ? "dark" : ""}>
+            <img className="nav-logo" alt="logo" src={logo} />
             <h1>ReactFacts</h1>
-            <h3>React Course - Project 1</h3>
+            <h3>React Course - Project 1/ Update 3</h3>
+
+            <div className="toggler">
+                <p className="toggler-light">Light</p>
+                <div className="toggler-slider" onClick={props.toggleDarkMode}>
+                    <div className="toggler-slider-circle"></div>
+                </div>
+                <p className="toggler-dark">Dark</p>
+            </div>
         </nav>
     )
 }
